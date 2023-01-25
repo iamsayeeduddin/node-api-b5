a4aa6c23347e88881f631fb3fc9446485fff3d14
Sayeeduddin <iamsayeeduddin@outlook.com>
2023-01-25 10:53:13 +0530

    Day 3

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const PORT = 5500;
 // Local Host - 127.0.0.1
 
 const handler = (req, res) => {
+  const books = [
+    { id: 1, name: "Code Optimization", price: 100 },
+    { id: 2, name: "Code Refactoring", price: 80 },
+    { id: 3, name: "CSS Flexbox", price: 50 },
+  ];
+  //routing
+  // setTimeout, setInterval,....
   switch (req.url) {
     case "/":
       res.write("Hello Node JS");
       break;
     case "/books":
-      res.write("Hello Books!");
+      res.write(JSON.stringify(books));
       break;
     case "/authors":
       res.write("Hello Authors!");
